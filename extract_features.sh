d9f9bdb0fc43fcda398a43d1e72297a8deca20b5
Yan (Eric) Wang <yw763@cornell.edu>
2019-04-10 12:04:59 -0400

    Update extract_features.sh

--- extract_features.sh
+++ extract_features.sh
@@ -10,10 +10,10 @@ gen_stage_features=$7
 
 if [[ ${gen_stage_features} == 'True' ]]; then
     CUDA_VISIBLE_DEVICES=${GPU} python main.py --arch ${nettype}  --dataset ${dataset} -j 4  -b 64 --crop_size 256 128 \
-    --data ${dataset_path} --extract_features --resume ~/checkpoints/$checkpoint_name/checkpoint.pth.tar \
+    --data ${dataset_path} --extract_features --resume ./checkpoints/$checkpoint_name/checkpoint.pth.tar \
     --extract_features_folder ${feature_path} --ten_crop
 else
     CUDA_VISIBLE_DEVICES=${GPU} python main.py --arch ${nettype}  --dataset ${dataset} -j 4  -b 64 --crop_size 256 128 \
-    --data ${dataset_path} --extract_features --resume ~/checkpoints/$checkpoint_name/checkpoint.pth.tar \
+    --data ${dataset_path} --extract_features --resume ./checkpoints/$checkpoint_name/checkpoint.pth.tar \
     --extract_features_folder ${feature_path} --ten_crop
 fi
